9ce45563cc8a3f08ada9350a2e9a7d064e8d2710
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-02 16:24:51 +0200

    commenti per aiuto personale

--- classes/movie.php
+++ classes/movie.php
@@ -1,15 +1,19 @@
 <?php
 
 // Definizione della CLASSE Movie
-class Movie {
+// La classe è lo scheletro o il modello che definisce le caratteristiche e i comportamenti che gli oggetti creati da essa avranno.
+class Movie
+{
     // Dichiarazione delle VARIABILI D'ISTANZA
+    // Proprietà Sono variabili che memorizzano dati relativi all'oggetto
     public $titolo;
     public $anno;
     public $lingua;
 
     // COSTRUTTORE della classe Movie
     // INIZIALIZZA le variabili d'istanza con i VALORI passati come PARAMETRI
-    public function __construct($_titolo, $_anno, $_lingua) {
+    public function __construct($_titolo, $_anno, $_lingua)
+    {
         $this->titolo = $_titolo;  // INIZIALIZZA la variabile titolo
         $this->anno = $_anno;      // INIZIALIZZA la variabile anno
         $this->lingua = $_lingua;  // INIZIALIZZA la variabile lingua
@@ -17,3 +21,4 @@ class Movie {
 }
 
 ?>
+<!-- Quando crei un oggetto, puoi assegnare qualsiasi valore desideri alle proprietà. Questi valori possono essere diversi per ogni oggetto creato dalla stessa classe! -->
--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 require_once __DIR__ . '/classes/movie.php';
 
 // Creazione di un'ISTANZA della classe Movie con specifici VALORI per titolo, anno e lingua
-$movie = new Movie(_titolo:"titolo del film", _anno:"anno del film", _lingua:"lingua del film");
+$movie = new Movie(_titolo: "The Hobbit", _anno: "1999", _lingua: "hobbitese");
 
 // Stampa dei VALORI delle PROPRIETà dell'OGGETTO Movie
-echo('Titolo del film: ' . $movie->titolo . '  ' . 'Lingua del film: ' . $movie->lingua . '  ' . 'Anno di uscita: ' . $movie->anno);
-?>
+echo ('Titolo del film: ' . $movie->titolo . '  ' . 'Lingua del film: ' . $movie->lingua . '  ' . 'Anno di uscita: ' . $movie->anno);
+echo '<br>';
+
+// puoi creare quanti OGGETTI vuoi partendo dalla CLASSE in movie.php, ogni oggetto può avere i VALORI delle PROPRIETà diversi dall'oggetto precedente(IN SOSTANZA LA CLASSE è LO SCHELETRO, E QUI NE DEFINISCO LE CARATTERISTICHE)
+$movie2 = new Movie(_titolo: "The Shark", _anno: 2022, _lingua: "Inglese");
+echo ('Titolo del film:' . $movie2->titolo . ' ' . 'anno di uscita:' . $movie2->anno . ' ' . 'lingua del film:' . $movie2->lingua);
