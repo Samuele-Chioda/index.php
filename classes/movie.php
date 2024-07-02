efd33d48afb5787576022b8f060a54d93da6b7a4
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-02 15:47:59 +0200

    aggiunto commenti personali per memorizzare concetti

--- classes/movie.php
+++ classes/movie.php
@@ -1,19 +1,19 @@
 <?php
 
-class Movie{
+// Definizione della CLASSE Movie
+class Movie {
+    // Dichiarazione delle VARIABILI D'ISTANZA
     public $titolo;
     public $anno;
     public $lingua;
 
-    public function __construct($_titolo, $_anno, $_lingua){
-        $this->titolo = $_titolo;
-        $this->anno = $_anno;
-        $this->lingua = $_lingua;
+    // COSTRUTTORE della classe Movie
+    // INIZIALIZZA le variabili d'istanza con i VALORI passati come PARAMETRI
+    public function __construct($_titolo, $_anno, $_lingua) {
+        $this->titolo = $_titolo;  // INIZIALIZZA la variabile titolo
+        $this->anno = $_anno;      // INIZIALIZZA la variabile anno
+        $this->lingua = $_lingua;  // INIZIALIZZA la variabile lingua
     }
-
 }
 
-
-
-
-
+?>
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-// collego file movie.php
+// COLLEGAMENTO del file movie.php che contiene la definizione della CLASSE Movie
 require_once __DIR__ . '/classes/movie.php';
 
+// Creazione di un'ISTANZA della classe Movie con specifici VALORI per titolo, anno e lingua
 $movie = new Movie(_titolo:"titolo del film", _anno:"anno del film", _lingua:"lingua del film");
 
-echo('titolo del film:' . $movie->titolo . 'lingua del film:' . $movie->lingua . 'anno di uscita:' . $movie->anno);
+// Stampa dei VALORI delle PROPRIETà dell'OGGETTO Movie
+echo('Titolo del film: ' . $movie->titolo . '  ' . 'Lingua del film: ' . $movie->lingua . '  ' . 'Anno di uscita: ' . $movie->anno);
 ?>
